6a607abc199e2050783cb7542d6b4e1ccadec43d
HakiPintolli <155005609+HakiPintolli@users.noreply.github.com>
2024-05-19 00:19:34 +0200

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,21 @@
+package so;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class main{
+    public static void main(String[] args) {
+        ProcessSync sync = new ProcessSync();
+
+        Thread threadA = new Thread(() -> {
+            sync.processA();
+        });
+
+        Thread threadB = new Thread(() -> {
+            sync.processB();
+        });
+
+        threadA.start();
+        threadB.start();
+    }
+}
